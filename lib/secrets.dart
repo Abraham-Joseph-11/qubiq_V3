6fea46a7727e627ebfb2322f98a8e16291bac530
EmmiBot123 <eurekamindss@gmail.com>
2026-01-22 15:23:12 +0530

    exe.update

--- lib/secrets.dart
+++
@@ -1,4 +0,0 @@
-// lib/secrets.dart
-const String googleGeminiApiKey = 'AIzaSyAnXNRiozr7Z9schC7JudlAqIwK8gwr7qs';
-const String geminiApiKey = 'AIzaSyCbuw1wrAKR9NWk9-duDgcj-_EOBTqCm-0';
-const String openRouterApiKey = "sk-or-v1-xxxxxxxx...";
